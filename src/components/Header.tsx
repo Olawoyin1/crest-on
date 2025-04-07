faa03f14eccdd7cd8713ccedcc74c6f07719f3de
Olawoyin <yustee2017@gmail.com>
2025-04-07 19:24:56 +0100

    Projec setuo completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src="../../Images/logo.png" alt="logo image" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
