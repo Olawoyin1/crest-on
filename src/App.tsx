2cb230977a4909e6d49382ca07db2797a8f0babc
Olawoyin <yustee2017@gmail.com>
2025-04-13 16:10:12 +0100

    footer done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import "./App.css";
-import Header from "./components/Header";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
+import SharedLayout from "./components/SharedLayout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
 
 function App() {
+  
+  
+  const main = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home/>} />
+  
+          <Route path="*" element={<ErrorPage />} />
+  
+          <Route path="services" element={<Services />} />
+        </Route>
+        )
+      )
   return (
-    <>
-     <Header />
-    </>
+    <RouterProvider router={main} />
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { GrHomeRounded } from "react-icons/gr";
+import { HiOutlineUsers } from "react-icons/hi";
+import { MdOutlineCalendarToday } from "react-icons/md";
+import { FiMessageSquare } from "react-icons/fi";
+import { MdOutlineCreditCard } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex bg-white justify-between items-center rounded-2xl mt-3 px-4 w-300 mx-auto'>
         <div>
             <img src="../../Images/logo.png" alt="logo image" />
+        </div>
+        <div>
+            <ul className='p-0 flex gap-2'>
+                <li className='flex items-center gap-1 '><GrHomeRounded /> Home</li>
+                <li className='flex items-center gap-1 '><HiOutlineUsers /> Patients</li>
+                <li className='flex items-center gap-1 '><MdOutlineCalendarToday /> Schedule</li>
+                <li className='flex items-center gap-1 '><FiMessageSquare /> Message</li>
+                <li className='flex items-center gap-1 '><MdOutlineCreditCard /> Transactions</li>
+            </ul>
+        </div>
+
+        <div className='flex items-center'>
+            <div>
+                <img src="../../Images/avatar.png" className='w-10' alt="" />
+            </div>
+            <div className="">
+                <p>Dr. Jones Simons</p>
+                <p>General Practinioners</p>
+            </div>
+            <p>|</p>
+            <IoSettingsOutline />
+            <BsThreeDotsVertical />
+
         </div>
     </div>
   )
